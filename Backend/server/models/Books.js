429ba0187993cca37517335b9247e4fe4ff3d0b1
PalfiVlastimil <vlastimil.palfi92@gmail.com>
2024-10-12 11:39:20 +0200

    expand book model

--- Backend/server/models/Books.js
+++ Backend/server/models/Books.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose")
 
 const BookSchema = new mongoose.Schema( {
+    isbn13: String,
+    isbn10: String,
     title: String,
-    author: String,
-    genre: String,
-    year: Number,
-    pages: Number
+    categories: String,
+    subtitles: String,
+    authors: String,
+    thumbnail: String,
+    description: String,
+    published_year: String,
+    average_rating: String,
+    num_pages: Number,
+    ratings_count: Number
 })
 const BookModel = mongoose.model("books", BookSchema)
 module.exports = BookModel;
