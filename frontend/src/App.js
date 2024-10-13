9a4b2382f6b7b172ca59babbf8bc59abf744c0df
Vítězslav Musil <vitezslav.musil@tul.cz>
2024-10-13 20:19:32 +0200

    Basic filtering logic + Pagenation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,26 +6,45 @@ import './App.css';
 import { useEffect, useState } from "react"
 import axios from "axios"
 function App() {
-  const [books, setBooks] = useState([])
+  const [books, setBooks] = useState([]);
+  const [page, setPage] = useState(1); // Current page
+  const [totalPages, setTotalPages] = useState(1); // Total pages
+  const [author, setAuthor] = useState('Bruce Patton'); // Author filter
+  const [genre, setGenre] = useState(''); // Genre filter
+  const [loading, setLoading] = useState(false);
+  const limit = 10; // Number of books per page
   const [error, setError] = useState(null);
-  const loadBooksData = async () =>{
-    axios.get("http://localhost:8002/getBooks")
+  const filter = async () => {
+    let link = "http://localhost:8002/getBooks?"
+    if (author != '') {
+      link = link + "author=${author}&"
+    }
+    if (genre != '') {
+      link = link + "genre=${genre}&"
+    }
+    console.log(link);
+    return link; 
+  }
+  const loadBooksData = async (link) =>{
+    axios.get(await link)
     .then(books =>  {
       setBooks(books.data.bookArray)
+      setTotalPages(books.data.totalPages); // Set total pages
     })
     .catch(err => {
       setError(error)
     })
   }
   useEffect(() =>{
-    loadBooksData();
-  }, [])
+    let link=filter();
+    loadBooksData(link);
+  },[]);
+  
   
   return (
     <div>
-      {console.log(error)}
       <Header />
-      <Main books={books}/>
+      <Main books={books} setPage={setPage} page={page} totalPages={totalPages} setAuthor={setAuthor} setGenre={setGenre}/>
       <Footer />
     </div>
   );
