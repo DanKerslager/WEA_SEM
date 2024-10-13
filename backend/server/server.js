82c617e46d7ec122d2580786a917888b355ce63e
Vítězslav Musil <vitezslav.musil@tul.cz>
2024-10-14 00:03:53 +0200

    Frontend and Database Sync (Paging and Filters) (#10)
    * Search bar
    
    * URI Encoding
    
    * Slight touches
    
    * Code Clarification
    
    * filter done!
    
    * Database Sync
    
    ---------
    
    Co-authored-by: PalfiVlastimil <vlastimil.palfi92@gmail.com>

--- backend/server/server.js
+++ backend/server/server.js
@@ -25,6 +25,8 @@ app.get("/books", async (req, res) => {
     // proměnné pro filtraci
     let author = req.query.author
     let genre = req.query.genre
+    let title = req.query.title
+    console.log(title)
     //Vytvoř filter objekt, query parametrů poslané přes URL
     let filter = {};
     if (author) {
@@ -32,6 +34,9 @@ app.get("/books", async (req, res) => {
     }
     if (genre) {
       filter.genre = genre; // Filtrace žánru
+    }
+    if (title) {
+      filter.title = title; // Filtrace názvu
     }
     //vypočítání stránkování
     const skip = (page - 1) * limit;
