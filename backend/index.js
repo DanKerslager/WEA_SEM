df8a66b1b3f43a69e11680221dfedd274ab4501d
Dan_Kerslager <dan.kerslager@tul.cz>
2024-10-21 11:06:21 +0200

    Linted with: ESLint

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const logger = require('./logger');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json({ limit: '50mb' })); 
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // MongoDB connection URI
