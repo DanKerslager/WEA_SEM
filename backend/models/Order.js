4ac088ddd424c40d1533ebf445b4e5a6b4149215
Dan_Kerslager <dan.kerslager@tul.cz>
2024-11-26 09:53:04 +0100

    CreateOrder route

---
+++ backend/models/Order.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const { AddressSchema } = require("./Schemes");
+
+const UserData = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    shippingAddress: AddressSchema,
+    billingAddress: AddressSchema,
+});
+
+const BookData = new mongoose.Schema({
+    isbn13: { type: String, required: true, minlength: 13, maxlength: 13, unique: true },
+    isbn10: { type: String, required: true, minlength: 10, maxlength: 10, unique: true },
+    title: { type: String, required: true, maxlength: 255 },
+    price: { type: Number, required: true, min: 0, set: (v) => Math.round(v * 100) / 100 },
+});
+
+const OrderSchema = new mongoose.Schema({
+    user: UserData,
+    items: [
+        {
+        book: BookData,
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1,
+        },
+        },
+    ],
+    orderDate: {
+        type: Date,
+        default: Date.now,
+    },
+    shipped: {
+        type: Boolean,
+        default: false,
+    },
+    payment: { type: String, required: true },
+    price: {
+        type: Number,
+        required: true,
+        min: 0,
+        set: (v) => Math.round(v * 100) / 100,
+    },
+    total: {
+        type: Number,
+        required: true,
+        min: 0,
+        set: (v) => Math.round(v * 100) / 100,
+    },
+    });
