a279339142cdf16af0228058ad52515934535928
Dan_Kerslager <dan.kerslager@tul.cz>
2024-12-05 11:23:37 +0100

    Added swagger doc

--- backend/routes/getOrders.js
+++ backend/routes/getOrders.js
@@ -1,3 +1,92 @@
+/**
+ * @swagger
+ * /{userId}/orders:
+ *   get:
+ *     summary: Retrieve all orders for a specific user
+ *     tags: [Orders]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The unique ID of the user
+ *         example: "605c72d7e25e5c23a8a5f2b4"
+ *     responses:
+ *       200:
+ *         description: A list of orders for the user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     description: Order ID
+ *                     example: "5f8d04a4b54764421b7156d3"
+ *                   user:
+ *                     type: object
+ *                     description: User details
+ *                     properties:
+ *                       _id:
+ *                         type: string
+ *                         description: User ID
+ *                         example: "605c72d7e25e5c23a8a5f2b4"
+ *                       name:
+ *                         type: string
+ *                         description: User's name
+ *                         example: "John Doe"
+ *                   items:
+ *                     type: array
+ *                     description: List of items in the order
+ *                     items:
+ *                       type: object
+ *                       properties:
+ *                         book:
+ *                           type: object
+ *                           description: Book details
+ *                           properties:
+ *                             title:
+ *                               type: string
+ *                               example: "Book Title"
+ *                             price:
+ *                               type: number
+ *                               example: 19.99
+ *                         quantity:
+ *                           type: number
+ *                           example: 2
+ *                   payment:
+ *                     type: string
+ *                     description: Payment method
+ *                     example: "Dobírka"
+ *                   total:
+ *                     type: number
+ *                     description: Total price
+ *                     example: 39.99
+ *       400:
+ *         description: User ID is required
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "User ID is required."
+ *       500:
+ *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Server error"
+ */
+
 const express = require('express');
 const orderController = require('../controllers/orderController');
 
