762f9d355dcdd598ae9deae1f95a21faee36d2a6
PalfiVlastimil <vlastimil.palfi92@gmail.com>
2024-12-08 10:14:44 +0100

    a bit of order styling

--- frontend/src/components/Pages/OrdersPage.jsx
+++ frontend/src/components/Pages/OrdersPage.jsx
@@ -40,18 +40,36 @@ const OrdersPage = ({ userId }) => {
             <h1>{t('orders')}</h1>
             <Accordion defaultIndex={[0]} allowMultiple>
                 {orders.map((order) => (
-                    <AccordionItem>
+                    <AccordionItem >
                         <Box key={order._id}>
-                            <AccordionButton>
+                            <AccordionButton id="order-item">
                                 <h2>#{order._id}</h2>
+                                <div>
+                                    <p>{t('status')}:</p>
+                                    <p>{order.status}</p>
+                                </div>
+                                <div>
+                                    <p>{t('payment_method')}:</p>
+                                    <p>{order.payment}</p>
+
+                                </div>
+                                <div>
+                                    <p>{t('total')}:</p>
+                                    <p>{order.price}</p>
+                                </div>
+                                <div>
+                                    <p>{t('total_tax')}:</p>
+                                    <p> {order.total}</p>
+                                </div>
+                                <div>
+                                    <div>
+                                        <p>{changeDateFormat(order.orderDate)}</p>
+                                    </div>
+                                </div>
                                 <AccordionIcon />
                             </AccordionButton>
                             <AccordionPanel>
-                                <p>{t('status')}: {order.status}</p>
-                                <p>{t('payment_method')}: {order.payment}</p>
-                                <p>{t('total')}: {order.price}</p>
-                                <p>{t('total_tax')}: {order.total}</p>
-                                <p>{changeDateFormat(order.orderDate)}</p>
+
                                 {order.items.map((item) => (
                                     <Box key={item._id}>
                                         <h3>{item.book.title}</h3>
@@ -66,8 +84,8 @@ const OrdersPage = ({ userId }) => {
                                 ))}
                             </AccordionPanel>
                         </Box>
-                        </AccordionItem>
-                    ))}
+                    </AccordionItem>
+                ))}
             </Accordion>
         </Box>
     )
